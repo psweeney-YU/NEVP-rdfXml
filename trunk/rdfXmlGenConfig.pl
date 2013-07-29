0fd201de4ec162479c93135d7f629bbfdf69dd6b
sweeneyyu <sweeneyyu@e28627f3-55a7-48d5-a04a-dbe7da5f16d1>
2013-07-29 14:20:02 +0000

    Added file copy functionality, ISO dates, missing info check, etc.

--- trunk/rdfXmlGenConfig.pl
+++ trunk/rdfXmlGenConfig.pl
@@ -3,13 +3,15 @@
 use strict;
 
 #appratus mysql database connection variables
-host	=> '130.132.27.5',
-db		=> 'NEVP_apparatus_4',
-user	=> 'generalUser',
-pass	=> 'neVpTcN91!',
+host	=> '',
+db		=> '',
+user	=> '',
+pass	=> '',
 
 #paths
-#provide locations to where RDF/XML data and log file should be written. Path is terminated with filename.
-specimenDataOutfile		=> '/Users/psweeney/Desktop/mysql_XML/rdfSpecimen',
-imageDataOutfile		=> '/Users/psweeney/Desktop/mysql_XML/rdfImage',
-logFile					=> '/Users/psweeney/Desktop/mysql_XML/rdfXmlLog',
+#provide path (ending in slash) to where RDF/XML metadata & images and path + filename for logfile.
+exportPath		=> '', #/path/to/export/directory/
+logFile			=> '', #/path/to/logfile/logfile_name
+
+#timezone. use Date::Manip 6.xx abbreviations: http://search.cpan.org/~sbeck/Date-Manip-6.40/lib/Date/Manip/Zones.pod.
+timezone	=> 'America/New_York';
