8bf27552fd15180180b867d0a41391412a10d22a
sweeneyyu <sweeneyyu@e28627f3-55a7-48d5-a04a-dbe7da5f16d1>
2013-08-20 21:00:47 +0000

    initial commit

---
+++ trunk/rdfXmlGenConfig.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+#appratus mysql database connection variables
+host	=> '',
+db		=> '',
+user	=> '',
+pass	=> '',
+
+#paths
+#provide path to where RDF/XML metadata & images and path + filename for logfile.
+exportiPlant		=> '/Users/psweeney/Desktop/mysql_XML/images/sink/iPlant/', #/path/to/iPlant/export/directory/
+exportSymbiota	=> '/Users/psweeney/Desktop/mysql_XML/images/sink/Symbiota/', #/path/to/Symbiota/export/directory/
+logPath			=> '/Users/psweeney/Desktop/mysql_XML/images/sink/iPlant/', #/path/to/logfile/logfile_name
+
+#timezone. use Date::Manip 6.xx abbreviations: http://search.cpan.org/~sbeck/Date-Manip-6.40/lib/Date/Manip/Zones.pod.
+timezone	=> 'America/New_York';
